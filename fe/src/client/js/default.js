58f34d17f71c2799318bcba9eac9e3dc90425b3e
xoxosoii <puiuxq@icloud.com>
2023-11-08 21:03:24 +0900

    다중 이벤트 리스너 작성

--- fe/src/client/js/default.js
+++ fe/src/client/js/default.js
@@ -20,16 +20,53 @@ const commonFunction = _ => {
 const rotateMenu = _ => {
 	const 
 		circle = document.querySelector(".foot-menu-circle")
-	,	menus = circle.querySelectorAll("a, button")
+	,	menus = circle.querySelectorAll("li")
 	,	radius = circle.clientWidth / 2
+	,	one_rad = 360 / menus.length
 	,	distance = Math.round( radius * 0.75 );
 	;
 
-	const init = _ => {
-		console.log( radius );
-		menus.forEach(( menu, i ) => {
-			menu.style.setProperty("transform", `translate(0, -${distance}px)`);
+	const setEvents = function( events, callback ) {
+		const 
+			isNodeList = NodeList.prototype.isPrototypeOf( this )
+		,	elements = isNodeList ? Array.prototype.slice.call( this ) : [this];
+		;
+
+		elements.forEach( elem => {
+			events.forEach( evt => {
+				elem.addEventListener( evt, callback )
+			});
 		});
+	};
+
+	NodeList.prototype.addEventsListener = setEvents;
+	HTMLElement.prototype.addEventsListener = setEvents;
+
+	const set_rotate = _ => {
+		return new Promise( resolve => {
+			menus.forEach(( menu, i ) => {
+				let rotate = (one_rad * i);
+				menu.style.setProperty("transform", `rotate(${ rotate }deg) translateY(-${ radius * 0.75 }px)`);
+			});
+			resolve();
+		});
+	};
+
+	const event_listener = _ => {
+		return new Promise( resolve => {
+			menus.addEventsListener(["mousedown", "touchstart"], _ => {
+				console.log("ok3");
+			});
+			circle.addEventsListener(["mousedown", "touchstart"], _ => {
+				console.log("ok3");
+			});
+			resolve();
+		});
+	};
+
+	const init = async _ => {
+		await set_rotate();
+		await event_listener();
 	};
 
 	(init)();
